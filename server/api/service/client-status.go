94814d3355d61be8f14fda6f0a8df940c33e02ff
huy <huy@nodeset.io>
2024-05-29 10:57:28 -0700

    revert

--- server/api/service/client-status.go
+++ server/api/service/client-status.go
@@ -51,14 +51,14 @@ func (c *serviceClientStatusContext) PrepareData(data *api.ServiceClientStatusDa
 
 	// Get the EC manager status
 	go func() {
-		ecMgrStatus := ec.CheckStatus(ctx, false)
+		ecMgrStatus := ec.CheckStatus(ctx, true)
 		data.EcManagerStatus = *ecMgrStatus
 		wg.Done()
 	}()
 
 	// Get the BC manager status
 	go func() {
-		bcMgrStatus := bc.CheckStatus(ctx, false)
+		bcMgrStatus := bc.CheckStatus(ctx, true)
 		data.BcManagerStatus = *bcMgrStatus
 		wg.Done()
 	}()
