ea9aa13645553f82dec233530d6b00aa0d6e34f1
huy <huy@nodeset.io>
2024-07-09 13:53:16 -0700

    clean up test

--- internal/tests/api/minipool_count_test.go
+++ internal/tests/api/minipool_count_test.go
@@ -18,19 +18,9 @@ func TestGetMinipoolCount(t *testing.T) {
 
 	nsMgr := testMgr.GetNodeSetMockServer().GetManager()
 	response, err := nsMgr.GetAvailableConstellationMinipoolCount(common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
-	// response, err := testMgr.GetApiClient().NodeSet_Constellation.GetAvailableMinipoolCount()
+
 	require.NoError(t, err)
 	require.Equal(t, 10, response)
 	t.Log("GetAvailableMinipoolCount called")
 
-	// // Run the round-trip test
-	// derivationPath := string(wallet.DerivationPath_Default)
-	// index := uint64(0)
-	// response, err := testMgr.GetApiClient().Wallet.Recover(&derivationPath, keys.DefaultMnemonic, &index, goodPassword, true)
-	// require.NoError(t, err)
-	// t.Log("Recover called")
-
-	// // Check the response
-	// require.Equal(t, expectedWalletAddress, response.Data.AccountAddress)
-	// t.Log("Received correct wallet address")
 }
